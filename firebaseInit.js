b03da2176507ca8c29b76e0db780c34ff59b3b6a
corasan <henrypl360@gmail.com>
2016-07-22 14:06:17 -0400

    firebase config and retrieve array of posts

---
+++ firebaseInit.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const config = firebase.initializeApp({
+    apiKey: "AIzaSyCEQx873k19FK4qbzHgGVFKyCGH5uyepkU",
+    authDomain: "lotus-app-1a2db.firebaseapp.com",
+    databaseURL: "https://lotus-app-1a2db.firebaseio.com",
+    storageBucket: "lotus-app-1a2db.appspot.com",
+});
+
+export default config;
---
+++ src/scenes/Posts.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import firebase from '../../firebaseInit';
+
+export default class Posts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+    componentWillMount() {
+        firebase.database().ref('Posts').on('value', (data) => {
+
+        });
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
