d44a96c6c658accbb9d4b2fe2ec8f378ee1e63dd
corasan <henrypl360@gmail.com>
2016-08-03 01:02:41 -0400

    showposts now show the title and content of the post

--- src/scenes/ShowPost.js
+++ src/scenes/ShowPost.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, ListView, TouchableHighlight } from 'react-native';
 
 export default class ShowPost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            content: ''
+        }
+    }
+
+    componentWillMount() {
+        firebase.database().ref('posts/'+this.props.id).once('value')
+        .then((data) => {
+            let post = data.val();
+            this.setState({title: post.title, content: post.text});
+        });
+    }
+
     render() {
         return (
-            <Text>{this.props.id}</Text>
+            <View>
+                <Text>{this.state.title}</Text>
+                <Text>{this.state.content}</Text>
+            </View>
         );
     }
 }
