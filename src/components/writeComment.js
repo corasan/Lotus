ec8d7ddf3b089630d3015ba3af5ff92bfc1b5138
corasan <henrypl360@gmail.com>
2016-08-03 18:59:12 -0400

    new firebase account

---
+++ src/components/writeComment.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native';
+
+export default class WriteComment extends Component {
+    render() {
+        return (
+            <TextInput />
+        );
+    }
+}
