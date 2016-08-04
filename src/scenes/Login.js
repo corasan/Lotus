7b42d8eba5961d3d190ae9a09fe4c5f3678e72f6
corasan <henrypl360@gmail.com>
2016-08-04 11:55:37 -0400

    login working

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Alert, TextInput } from 'react-native';
 
 export default class Login extends Component {
     constructor(props) {
@@ -10,7 +10,15 @@ export default class Login extends Component {
         }
     }
 
-    Login = () => {
+    handleEmail = (email) => {
+        this.setState({email});
+    }
+
+    handlePassword = (password) => {
+        this.setState({password});
+    }
+
+    login = () => {
         firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
         .then(() => this.props.navigator.resetTo({name: 'Posts'}) )
         .catch((error) => {
@@ -20,7 +28,28 @@ export default class Login extends Component {
 
     render() {
         return (
-            <Text>Login page</Text>
+            <View>
+                <Text>Login page</Text>
+                <TextInput value={this.state.email} onChangeText={this.handleEmail}/>
+                <TextInput value={this.state.password} onChangeText={this.handlePassword}/>
+
+                <TouchableHighlight onPress={this.login} style={styles.loginBtn}>
+                    <Text>Login</Text>
+                </TouchableHighlight>
+            </View>
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    loginBtn: {
+        marginTop: 40,
+        height: 60,
+        width: 160,
+        backgroundColor: '#00BFA5',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 4
+    },
+});
