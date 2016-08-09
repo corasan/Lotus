a79ceb6ec0a131d62b5886a7df3dae9fea6444cb
corasan <henrypl360@gmail.com>
2016-08-09 17:05:13 -0400

    changes to navbar

--- index.android.js
+++ index.android.js
@@ -12,12 +12,12 @@ import {
 } from 'react-native';
 import Posts from './src/scenes/Posts';
 import firebase from './firebaseInit';
-import navbar from './src/components/Navigation/navbar';
 import NewPost from './src/scenes/NewPost';
 import ShowPost from './src/scenes/ShowPost';
 import Login from './src/scenes/Login';
 import Signup from './src/scenes/Signup';
 import { Actions, Router, Scene, ActionConst } from 'react-native-router-flux';
+import NavigationBar from './src/components/Navigation/NavigationBar';
 
 let navigator;
 
@@ -49,7 +49,9 @@ class Lotus extends Component {
 
     render() {
         return (
-            <Router sceneStyle={styles.container}>
+            <Router sceneStyle={styles.container} navigationBarStyle={styles.navbar}
+                backButtonImage={require('./src/img/left.png')}
+            >
                 <Scene key="root">
                     <Scene key="login" component={Login} initial={true} hideNavBar={true}/>
                     <Scene key="posts" component={Posts} hideNavBar={false} type={ActionConst.REPLACE} tabs={true}/>
@@ -63,16 +65,21 @@ class Lotus extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EFEBE9',
-    marginTop: 55,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#EFEBE9',
+        marginTop: 55,
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    navbar: {
+        backgroundColor: '#00BFA5',
+        elevation: 3,
+        borderBottomWidth: 0,
+    }
 });
 
 AppRegistry.registerComponent('Lotus', () => Lotus);
