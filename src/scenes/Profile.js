c628fe2ebd4ca1ec496d1552ea28a1da8f424d6c
corasan <henrypl360@gmail.com>
2016-08-15 12:58:00 -0400

    removed inline styles from profile, more organized

--- src/scenes/Profile.js
+++ src/scenes/Profile.js
@@ -28,27 +28,23 @@ export default class Profile extends Component {
         }.bind(this));
     }
 
-    setProgress = (x) => {
-        this.setState({progress: (x/100)/100});
-    }
-
     render() {
         return (
             <View style={styles.container}>
-                <View style={{flexDirection: 'row', marginTop: 35}}>
+                <View style={styles.label}>
                     <Text style={styles.name}>{this.state.displayName}</Text>
-                    <Text style={{fontSize: 16, marginTop: 8, color: '#95989A'}}>Rank {this.state.rank}</Text>
+                    <Text style={[styles.subLabel, {marginTop: 8}]}>Rank {this.state.rank}</Text>
                 </View>
 
-                <View style={{flexDirection: 'row', marginTop: 35, justifyContent: 'space-between'}}>
+                <View style={[styles.label, {justifyContent: 'space-between'}]}>
                     <Score title="Reputation" score={this.state.reputation}/>
                     <Score title="Posts" score={this.state.posts}/>
                     <Score title="Helped" score={this.state.helped}/>
                 </View>
 
-                <View style={{flexDirection: 'row', marginTop: 35}}>
-                    <Text style={{fontSize: 18, color: '#494D4F'}}>Next Rank</Text>
-                    <Image source={require('../img/levelUp.png')} style={{height: 30, width: 30, bottom: 4}}/>
+                <View style={styles.label}>
+                    <Text style={styles.labelName}>Next Rank</Text>
+                    <Image source={require('../img/levelUp.png')} style={styles.img}/>
                 </View>
                 <View style={{marginTop: 10}}>
                     <Progress.Bar
@@ -62,9 +58,9 @@ export default class Profile extends Component {
                     />
                 </View>
 
-                <View style={{marginTop: 35, flexDirection: 'row'}}>
-                    <Text style={{fontSize: 18, color: '#494D4F'}}>Medals</Text>
-                    <Text style={{fontSize: 16, color: '#95989A', marginTop: 3, marginLeft: 10}}>{this.state.medals}</Text>
+                <View style={styles.label}>
+                    <Text style={styles.labelName}>Medals</Text>
+                    <Text style={[styles.subLabel, {marginTop: 3}]}>{this.state.medals}</Text>
                 </View>
             </View>
         );
@@ -82,7 +78,24 @@ const styles = {
     name: {
         fontSize: 24,
         fontWeight: '900',
-        marginRight: 10,
         color: '#494D4F'
+    },
+    label: {
+        marginTop: 35,
+        flexDirection: 'row'
+    },
+    labelName: {
+        fontSize: 18,
+        color: '#494D4F'
+    },
+    subLabel: {
+        fontSize: 16,
+        color: '#95989A',
+        marginLeft: 10
+    },
+    img: {
+        height: 30,
+        width: 30,
+        bottom: 4
     }
 };
