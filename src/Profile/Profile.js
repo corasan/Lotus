b1c799d8a73c504f517a85ff97887de5f2579485
corasan <henrypl360@gmail.com>
2016-08-16 15:12:54 -0400

    readme

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import Score from './score';
 import * as Progress from 'react-native-progress';
-
+// TODO: Show earned medals
 export default class Profile extends Component {
     constructor(props) {
         super(props);
